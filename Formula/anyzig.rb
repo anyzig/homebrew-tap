2264ab09510e0bc5ff7c52aa4b3cfad6e63db723
Jonathan Marler <johnnymarler@gmail.com>
2025-08-12 08:19:28 -0600

    don't build anyzig, just use the prebuilt binary

--- Formula/anyzig.rb
+++ Formula/anyzig.rb
@@ -2,9 +2,10 @@ class Anyzig < Formula
   desc "A universal zig executable that runs any version of zig."
   homepage "https://github.com/marler8997/anyzig"
 
-  @@version = "v2025_08_03"
+  version "v2025_08_03"
 
-  url "https://github.com/marler8997/anyzig/archive/refs/tags/#{@@version}.tar.gz"
+  # Placeholder URL - actual binary is downloaded in install method based on architecture
+  url "https://github.com/marler8997/anyzig/archive/refs/tags/#{version}.tar.gz"
   sha256 "2600445976486c41944bbc73fc0f91d2bf91a5f5c4e56e394767722da934249b"
   license "MIT"
 
@@ -18,15 +19,12 @@ class Anyzig < Formula
             Hardware::CPU.arch.to_s
         end
 
-    bootstrap_url = "https://github.com/marler8997/anyzig/releases/download/#{@@version}/anyzig-#{arch}-macos.tar.gz"
+    binary_url = "https://github.com/marler8997/anyzig/releases/download/#{version}/anyzig-#{arch}-macos.tar.gz"
 
-    system "curl", "-L", "-o", buildpath/"anyzig.tar.gz", bootstrap_url
+    system "curl", "-L", "-o", buildpath/"anyzig.tar.gz", binary_url
     system "tar", "-xf", buildpath/"anyzig.tar.gz", "-C", buildpath
-    bootstrap_zig = buildpath/"zig"
 
-    args = []
-    args << "-Dcpu=#{cpu}" if build.bottle?
-    system bootstrap_zig, "build", *args, *std_zig_args
+    bin.install buildpath/"zig"
   end
 
   test do
